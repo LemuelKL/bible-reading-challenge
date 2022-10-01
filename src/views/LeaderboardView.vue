54c965be311bdec1b85f3ef9012ebf90022440b0
LemuelKL <lemuellee.kl@gmail.com>
2022-10-01 15:04:21 +0800

    Update LeaderboardView.vue

--- src/views/LeaderboardView.vue
+++ src/views/LeaderboardView.vue
@@ -1,2 +1,55 @@
-<script setup lang="ts"></script>
-<template><div>Leaderboard</div></template>
+<script setup lang="ts">
+import { supabase } from '@/supabase';
+import { ref } from 'vue';
+
+interface ReadingRecord {
+    userId: string;
+    readCount: number;
+}
+
+const userIds = ref<string[]>([]);
+const userReadingRecords = ref<ReadingRecord[]>([]);
+
+async function fetchReadCount(uid: string) {
+    const { data, error, count } = await supabase
+        .from('readings_done')
+        .select('*', { count: 'exact' })
+        .eq('reader', uid);
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    if (data && count) {
+        userReadingRecords.value.push({
+            userId: uid,
+            readCount: count
+        });
+    }
+}
+
+supabase
+    .from('profiles')
+    .select('id')
+    .then(({ data, error }) => {
+        if (error) {
+            console.error(error);
+        } else {
+            userIds.value = data.map((profile) => {
+                return profile.id;
+            });
+        }
+    })
+    .then(() => {
+        userIds.value.forEach((userId) => {
+            fetchReadCount(userId);
+        });
+    });
+</script>
+<template>
+    <div>Leaderboard</div>
+    <div v-for="record in userReadingRecords" :key="record.userId">
+        {{ record.userId }} {{ record.readCount }}
+    </div>
+</template>
