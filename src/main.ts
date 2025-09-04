ffa2628097cf142428f988dbfac1156697a2f71f
Lemuel Lee <lemuellee.kl@gmail.com>
2025-09-04 06:40:10 +0100

    override new shadow to keep style

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import '@quasar/extras/material-icons/material-icons.css';
 
 // Import Quasar css
 import 'quasar/src/css/index.sass';
+import '@/css/app-override.sass';
 
 import App from './App.vue';
 import router from './router';
